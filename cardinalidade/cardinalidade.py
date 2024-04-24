2d6eb3293909512feb42dcf09116447a2c839832
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2024-04-24 13:17:01 -0300

    ✨ feat: exercicios de cardinalidade

---
+++ cardinalidade/cardinalidade.py
@@ -0,0 +1,21 @@
+print("::::: Matemática Discreta - Cardinalidade :::::")
+
+#def contador(elementos):
+
+#    return elementos.strip().count(",") + 1
+#
+while True:
+    elementos = input("Informe os elementos do conjunto separando-os por uma virgula: ")
+    
+    elementosClean = elementos.replace(" ", "").split(",")
+    conjunto = set(elementosClean)
+    
+    cardinalidade = len(conjunto)
+    
+    print("O conjunto informado possui a cardinalidade =", cardinalidade)
+            
+    encerrarPrograma = input("Pressione '*' para sair ou qualquer outro caracter para informar novos elementos de um conjunto: ")    
+
+    if encerrarPrograma.lower() == "*":
+        print("Programa finalizado!")
+        break
