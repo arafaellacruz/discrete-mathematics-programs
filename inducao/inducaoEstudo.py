a8b838225800a38a519a217e8699dc2cd2aa64a6
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2024-06-21 16:33:03 -0300

    🐛fix: fixing bugs

--- inducao/inducaoEstudo.py
+++ inducao/inducaoEstudo.py
@@ -11,18 +11,18 @@ perguntas = [
 
     },
     {
-        "mensagem": "\nA 1ª parte de PPIM é a validação da Base, onde mostrar que a propriedade que se quer provar é válida para o menor valor possível do conjunto de números naturais. Geralmente, verifica-se se a propriedade é verdadeira para 𝑛=1n=1 ou 𝑛=0, quando não há uma condição para n. A afirmação está: \na) Verdadeiro \nb) Falso \nResposta: ",
-        "respostaCorreta" : 'b' or 'B',
+        "mensagem": "\nA 1ª parte de PPIM é a validação da Base, onde mostrar que a propriedade que se quer provar é válida para o \nmenor valor possível do conjunto de números naturais. Geralmente, verifica-se se a propriedade é verdadeira \npara n=1 ou n=0, quando não há uma condição para n. A afirmação está: \na) Verdadeiro \nb) Falso \nResposta: ",
+        "respostaCorreta" : 'a' or 'A',
         "acertos": "Parabéns, você acertou! (:\n"
 
     },
     {
-        "mensagem": "\nA 2ª parte de PPIM é a Hipostese Indutiva, onde supõe-se que a propriedade seja válida para um número natural k arbitrário, ou seja: \na) Assumimos que a propriedade é verdadeira para um valor n = k. \nb) Escolhemos um número natural qualquer para n. \nResposta: ",
+        "mensagem": "\nA 2ª parte de PPIM é a Hipostese Indutiva, onde supõe-se que a propriedade seja válida para um número \nnatural k arbitrário, ou seja: \na) Assumimos que a propriedade é verdadeira para um valor n = k. \nb) Escolhemos um número natural qualquer para n. \nResposta: ",
         "respostaCorreta" : 'a' or 'A',
         "acertos": "Parabéns, você acertou! (:\n"
     },
     {
-        "mensagem": "\nA 3ª parte de PPIM é a Tese, onde demonstramos que, se a propriedade é válida para n=k, então ela também é válida para n = k + 1, e essencialmente, mostra-se que a validade da propriedade para k implica a validade para k+1, e substituimos todo k por k + 1. A afirmação está:\na) Correta \nb) Incorreta \nResposta: ",
+        "mensagem": "\nA 3ª parte de PPIM é a Tese, onde demonstramos que, se a propriedade é válida para n=k, então ela também é \nválida para n = k + 1, e essencialmente, mostra-se que a validade da propriedade para k implica a validade \npara k+1, e substituimos todo k por k + 1. A afirmação está:\na) Correta \nb) Incorreta \nResposta: ",
         "respostaCorreta" : 'a' or 'A',
         "acertos": "Parabéns, você acertou! (:\n"
     },   
